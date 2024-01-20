3d1710e9c69f45cdc353b29f441177069fc81333
ReComplexed <lukasbeer987@gmail.com>
2024-01-20 16:42:57 +0100

    Add SFX Support and Merge Termux to Main

--- EpidemicDL.sh
+++ EpidemicDL.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
-zparseopts -D -E -F - -help=help h=help -no-stems=NoStems N=NoStems -full-search=FullSearch F=FullSearch T=top -top-result=top -no-metadata=NoMetadata M=NoMetadata -no-picture=NoPicture P=NoPicture -artist=Artist A=Artist -search=Search S=Search -skip-confirm=SkipConfirm C=SkipConfirm -remove-metadata=RemoveMetadata R=RemoveMetadata K=KeepPicture -keep-picture=KeepPicture
+zparseopts -D -E -F - -help=help h=help -no-stems=NoStems N=NoStems -full-search=FullSearch F=FullSearch T=top -top-result=top -no-metadata=NoMetadata M=NoMetadata -no-picture=NoPicture P=NoPicture -artist=Artist A=Artist -search=Search S=Search -skip-confirm=SkipConfirm C=SkipConfirm -remove-metadata=RemoveMetadata R=RemoveMetadata K=KeepPicture -keep-picture=KeepPicture -limit-search=LimitSearch L=LimitSearch -search-sfx=SearchSFX X=SearchSFX
 
 if [[ $help == "--help" ]] || [[ $help == "-h" ]] || [[ $1 == "" ]]
 then
@@ -14,6 +14,7 @@ then
 	echo " Usage: ./EpidemicDownload.sh [Options] <Search Option> <Search Term>"
 	echo ""
 	echo " --search / -S                  Search for the Music Name"
+	echo " --search-sfx / -X              Search for the Sound Effect Name"
 	echo " --artist / -A                  Enter an Artist Name"
 	echo ""
 	echo "-----------------------------------------------------"
@@ -28,6 +29,7 @@ then
 	echo " --skip-confirm / -C            Skip Asking the User if Search Result is correct"
 	echo " --no-picture / -P              Skip Downloading and Adding the Picture"
 	echo " --keep-picture / -K            Do not Delete Picture after adding it to the Audio Metadata"
+	echo " --limit-search / -L            Limit the Search Results (Default: 10)"
 	exit
 fi
 
@@ -41,15 +43,24 @@ then
 	then
 		1=${1// /-}
 		1=https://www.epidemicsound.com/artists/$1/
+	elif [[ $SearchSFX == "--search-sfx" ]] || [[ $SearchSFX == "-X" ]]
+	then
+		1=${1// /%20}
+		1=https://www.epidemicsound.com/sound-effects/search/?term=$1
 	fi
 fi
 
-if ! command -v eyeD3 &> /dev/null
+if command -v eyeD3 &> /dev/null
 then
+	eyeD3=true
+	binEyeD3=eyeD3
+elif [ -f /data/data/com.termux/files/usr/lib/python3.11/site-packages/eyed3/main.py ]
+then
+	eyeD3=true
+	binEyeD3=python /data/data/com.termux/files/usr/lib/python3.11/site-packages/eyed3/main.py
+else
 	eyeD3=false
     echo "eyeD3 could not be found. Skipping Metadata Changes."
-else
-	eyeD3=true
 fi
 
 if ! command -v wget &> /dev/null
@@ -57,6 +68,7 @@ then
     echo "wget could not be found."
     exit
 fi
+
 if ! command -v jq &> /dev/null
 then
     echo "jq could not be found."
@@ -68,25 +80,96 @@ then
     exit
 fi
 
+if [[ $DEBUG == "true" ]]
+then
+	echo "Binary Check Completed"
+	echo "INPUT: $1"
+fi
+
+if [[ $LimitSearch == "--limit-search" ]] || [[ $LimitSearch == "-L" ]]
+then
+	LimitSearch=$LimitSearch
+else
+	LimitSearch=10
+fi
 
 if [[ "$1" == "https://www.epidemicsound.com/artists/"* ]]
 then
+	if [[ $DEBUG == "true" ]]
+	then
+		echo "Artist URL Detected"
+	fi
 	JSONURL=${1/artists/"json/releases"}
 	declare -l JSONURL
 	if [[ $(curl -s $JSONURL) == "" ]]
 	then
-		echo "URL Not Found, Exiiting..."
+		echo "JSON URL Not Found, Exiting..."
 		exit
 	fi
 	ENTITIES=$(curl -s $JSONURL | jq -M -r '.entities.tracks')
 elif [[ "$1" == "https://www.epidemicsound.com/music/search/"* ]]
 then
+	if [[ $DEBUG == "true" ]]
+	then
+		echo "Music Search Detected"
+	fi
 	TERM=${1##*term=}
 	SEARCH=true
-	JSONURL=${1/music\/search/"json/search/tracks"}
+	JSONURL=${1/music\/search/"json/search/tracks"}\&translate_text=false\&order=desc\&sort=relevance\&limit=$LimitSearch
 	if [[ $(curl -s $JSONURL) == "" ]]
 	then
-		echo "URL Not Found, Exiiting..."
+		echo "JSON URL Not Found, Exiting..."
+		exit
+	fi
+	META=$(curl -s $JSONURL | jq -M -r '.meta.hits')
+	ID=${META:21}
+	ID=${ID%%,*}
+	if [[ $FullSearch == "--full-search" ]] || [[ $FullSearch == "-F" ]]
+	then
+		ENTITIES=$(curl -s $JSONURL | jq -M -r '.entities.tracks')
+	else
+		ENTITIES=$(curl -s $JSONURL | jq -M -r '.entities.tracks["'$ID'"]')
+	fi
+elif [[ "$1" == "https://www.epidemicsound.com/sound-effects/search/"* ]]
+then
+	if [[ $DEBUG == "true" ]]
+	then
+		echo "Sound Effect Search Detected"
+	fi
+	TERM=${1##*term=}
+	SEARCH=true
+	JSONURL=${1/sound-effects\/search/"json/search/sfx"}\&translate_text=false\&order=desc\&sort=relevance\&limit=$LimitSearch
+	if [[ $(curl -s $JSONURL) == "" ]]
+	then
+		echo "JSON URL Not Found, Exiting..."
+		exit
+	fi
+	META=$(curl -s $JSONURL | jq -M -r '.meta.hits')
+	ID=${META:21}
+	ID=${ID%%,*}
+	if [[ $FullSearch == "--full-search" ]] || [[ $FullSearch == "-F" ]]
+	then
+		ENTITIES=$(curl -s $JSONURL | jq -M -r '.entities.tracks')
+	else
+		ENTITIES=$(curl -s $JSONURL | jq -M -r '.entities.tracks["'$ID'"]')
+	fi
+elif [[ "$1" == "https://www.epidemicsound.com/sound-effects/"* ]]
+then
+	if [[ $DEBUG == "true" ]]
+	then
+		echo "Sound Effect Mood URL Detected"
+	fi
+	MOOD=$1
+	if [[ "${MOOD: -1}" == "/" ]]
+	then
+		MOOD=${MOOD::-1}
+	fi
+	MOOD=${MOOD##*/}
+	JSONURL=https://www.epidemicsound.com/json/search/sfx/?limit=$LimitSearch\&order=desc\&sort=relevance\&term=$MOOD
+	echo $JSONURL
+	if [[ $(curl -s $JSONURL) == "" ]]
+	then
+		echo "JSON URL Not Found, Exiting..."
 		exit
 	fi
 	META=$(curl -s $JSONURL | jq -M -r '.meta.hits')
@@ -100,6 +183,10 @@ then
 	fi
 elif [[ "$1" == "https://www.epidemicsound.com/track/"* ]]
 then
+	if [[ $DEBUG == "true" ]]
+	then
+		echo "Track URL Detected"
+	fi
 	JSONURL=${1/track/"json/track"}
 	if [[ "${JSONURL: -1}" == "/" ]]
 	then
@@ -107,11 +194,15 @@ then
 	fi
 	if [[ $(curl -s $JSONURL) == "" ]]
 	then
-		echo "URL Not Found, Exiiting..."
+		echo "JSON URL Not Found, Exiting..."
 		exit
 	fi
 	ENTITIES=$(curl -s $JSONURL | jq -M -r '.')
+else 
+	echo "Invalid URL, Exiting..."
+	exit
 fi
+
 AUDIO=$(echo $ENTITIES | grep 'lqMp3Url\|baseUrl\|title\|"L":')
 if [[ $NoMetadata != "--no-metadata" ]] && [[ $NoMetadata != "-M" ]] && [[ $RemoveMetadata != "--remove-metadata" ]] && [[ $RemoveMetadata != "-R" ]]
 then
@@ -227,59 +318,59 @@ do
 	then
 		if [[ $BASS == "true" ]]
 		then
-			eyeD3 --remove-all "$TITLE - Bass.mp3" &> /dev/null
+			$binEyeD3 --remove-all "$TITLE - Bass.mp3" &> /dev/null
 		fi
 		if [[ $DRUMS == "true" ]]
 		then
-			eyeD3 --remove-all "$TITLE - Drums.mp3" &> /dev/null
+			$binEyeD3 --remove-all "$TITLE - Drums.mp3" &> /dev/null
 		fi
 		if [[ $INSTRUMENTS == "true" ]]
 		then
-			eyeD3 --remove-all "$TITLE - Instruments.mp3" &> /dev/null
+			$binEyeD3 --remove-all "$TITLE - Instruments.mp3" &> /dev/null
 		fi
 		if [[ $MELODY == "true" ]]
 		then
-			eyeD3 --remove-all "$TITLE - Melody.mp3" &> /dev/null
+			$binEyeD3 --remove-all "$TITLE - Melody.mp3" &> /dev/null
 		fi
 	elif [[ $NoMetadata == "--no-metadata" ]] || [[ $NoMetadata == "-M" ]]
 	then
 		# Do Nothing
 	elif [[ $NoPicture == "--no-picture" ]] || [[ $NoPicture == "-P" ]]
 	then
-		eyeD3 -t "$TITLE" -a "$ARTIST" "$TITLE.mp3" &> /dev/null
+		$binEyeD3 -t "$TITLE" -a "$ARTIST" "$TITLE.mp3" &> /dev/null
 		if [[ $BASS == "true" ]]
 		then
-		eyeD3 -t "$TITLE" -a "$ARTIST" --add-comment "Bass" "$TITLE - Bass.mp3" &> /dev/null
+		$binEyeD3 -t "$TITLE" -a "$ARTIST" --add-comment "Bass" "$TITLE - Bass.mp3" &> /dev/null
 		fi
 		if [[ $DRUMS == "true" ]]
 		then
-			eyeD3 -t "$TITLE" -a "$ARTIST" --add-comment "Drums" "$TITLE - Drums.mp3" &> /dev/null
+			$binEyeD3 -t "$TITLE" -a "$ARTIST" --add-comment "Drums" "$TITLE - Drums.mp3" &> /dev/null
 		fi
 		if [[ $INSTRUMENTS == "true" ]]
 		then
-			eyeD3 -t "$TITLE" -a "$ARTIST" --add-comment "Instruments" "$TITLE - Instruments.mp3" &> /dev/null
+			$binEyeD3 -t "$TITLE" -a "$ARTIST" --add-comment "Instruments" "$TITLE - Instruments.mp3" &> /dev/null
 		fi
 		if [[ $MELODY == "true" ]]
 		then
-			eyeD3 -t "$TITLE" -a "$ARTIST" --add-comment "Melody" "$TITLE - Melody.mp3" &> /dev/null
+			$binEyeD3 -t "$TITLE" -a "$ARTIST" --add-comment "Melody" "$TITLE - Melody.mp3" &> /dev/null
 		fi
 	else
-		eyeD3 -t "$TITLE" -a "$ARTIST" --add-image "$TITLE.jpg":FRONT_COVER "$TITLE.mp3" &> /dev/null
+		$binEyeD3 -t "$TITLE" -a "$ARTIST" --add-image "$TITLE.jpg":FRONT_COVER "$TITLE.mp3" &> /dev/null
 		if [[ $BASS == "true" ]]
 		then
-			eyeD3 -t "$TITLE" -a "$ARTIST" --add-image "$TITLE.jpg":FRONT_COVER --add-comment "Bass" "$TITLE - Bass.mp3" &> /dev/null
+			$binEyeD3 -t "$TITLE" -a "$ARTIST" --add-image "$TITLE.jpg":FRONT_COVER --add-comment "Bass" "$TITLE - Bass.mp3" &> /dev/null
 		fi
 		if [[ $DRUMS == "true" ]]
 		then
-			eyeD3 -t "$TITLE" -a "$ARTIST" --add-image "$TITLE.jpg":FRONT_COVER --add-comment "Drums" "$TITLE - Drums.mp3" &> /dev/null
+			$binEyeD3 -t "$TITLE" -a "$ARTIST" --add-image "$TITLE.jpg":FRONT_COVER --add-comment "Drums" "$TITLE - Drums.mp3" &> /dev/null
 		fi
 		if [[ $INSTRUMENTS == "true" ]]
 		then
-			eyeD3 -t "$TITLE" -a "$ARTIST" --add-image "$TITLE.jpg":FRONT_COVER --add-comment "Instruments" "$TITLE - Instruments.mp3" &> /dev/null
+			$binEyeD3 -t "$TITLE" -a "$ARTIST" --add-image "$TITLE.jpg":FRONT_COVER --add-comment "Instruments" "$TITLE - Instruments.mp3" &> /dev/null
 		fi
 		if [[ $MELODY == "true" ]]
 		then
-			eyeD3 -t "$TITLE" -a "$ARTIST" --add-image "$TITLE.jpg":FRONT_COVER --add-comment "Melody" "$TITLE - Melody.mp3" &> /dev/null
+			$binEyeD3 -t "$TITLE" -a "$ARTIST" --add-image "$TITLE.jpg":FRONT_COVER --add-comment "Melody" "$TITLE - Melody.mp3" &> /dev/null
 		fi
 	fi
 	if [[ $NoMetadata != "--no-metadata" ]] && [[ $NoMetadata != "-M" ]] && [[ $NoPicture != "--no-picture" ]] && [[ $NoPicture != "-P" ]] && [[ $KeepPicture != "-K" ]] && [[ $KeepPicture != "--keep-picture" ]]
